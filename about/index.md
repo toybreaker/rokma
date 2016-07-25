dbfc48abed44cd65705e8c9baf1d5854d2d34f52
toybreaker <rokmabox@gmail.com>
2016-07-26 00:44:39 +0200

    cleaned much

---
+++ about/index.md
@@ -0,0 +1,10 @@
+---
+layout: about
+title: About
+bodyclass: about
+excerpt: "Can you hire a mind to benefit your company? Rokma's story demonstrate you definitively can!"
+---
+
+<blockquote>Can you hire a mind to benefit your company? Rokma's story demonstrate you definitively can! This guy has lived and worked all over the world and is familiar with a broad selection of cultural views. A natural maker, he has capably taken many projects from conception to completion, building startups before start-up times. An entrepreneur involved in publishing, advertising, visual communication and web design since the beginning he has always stayed current. Some of his works led the way, opening paths and setting new norms in their context.</blockquote>
+
+<cite>**Joan Epher, Founder of Surf Zombie Wax**</cite>
--- gulpfile.js
+++ gulpfile.js
@@ -1,100 +1,72 @@
 'use strict';
-// GULP here process sass and enable browsersync
-// sourcemap handled automagically by gulp-ruby-sass
 // images task to optimize images.
-
-// w/watch function now... and it works!!!!!
 var gulp         = require('gulp');
-var sass         = require('gulp-ruby-sass');
-var filter       = require('gulp-filter');
-var browserSync  = require('browser-sync');
-
-var rename       = require('gulp-rename');
 var responsive   = require('gulp-responsive');
 var imagemin     = require('gulp-imagemin');
-var pngquant     = require('imagemin-pngquant');
-var reload       = browserSync.reload;
 
 
-// Reponsive sizing
-gulp.task('images', function () {
-  return gulp.src('./src/images/*.jpg')
+// Reponsive sizing DEFAULT
+gulp.task('default', function () {
+  return gulp.src('./BIM/*.jpg')
     .pipe(responsive({
-    '*.jpg': [{
-        width:320,
-        quality: 51,
+      '*.jpg': [{
+        //nexus5
+        width: 640,
+        quality: 61,
         progressive: true,
         sharper: true,
         rename: {
-            suffix:'-320'
+          suffix: '-640'
         }
-    },{
-        width:640,
-        quality: 51,
-        progressive: true,
-        sharper: true,
-        rename: {
-            suffix:'-640'
-        }
-    },{
-        width:1024,
-        quality: 51,
+      }, {
+        //ipad
+        width: 1024,
+        quality: 66,
         progressive: true,
         rename: {
-            suffix:'-1024'
+          suffix: '-1024'
         }
-    },{
-        width:1680,
+      }, {
+        //average laptop screen
+        width: 1680,
+        quality: 66,
+        progressive: true,
+        rename: {
+          suffix: '-1680'
+        }
+      }, {
+        //fullHD
+        width: 1920,
         quality: 71,
         progressive: true,
         rename: {
-            suffix:'-1680'
+          suffix: '-1920'
         }
-    }]
+      }]
     }))
-    .pipe(gulp.dest('./assets/p'));
+    .pipe(gulp.dest('./BAM/'));
 });
 
-// Compress jpegs
-gulp.task('imagemin', function () {
-    return gulp.src('./assets/p/*.jpg')
-        .pipe(imagemin({
-            progressive: true
-        }))
-        .pipe(gulp.dest('./assets/p/min'));
+// variations on DEFAULT gulp task
+gulp.task('bim', function () {
+  return gulp.src('./BIM/*.jpg')
+    .pipe(responsive({
+      '*.jpg': [{
+        //average laptop screen
+        width: 1680,
+        quality: 33,
+        progressive: true,
+        rename: {
+          prefix: 'islander-1680-'
+        }
+      }]
+    }))
+    .pipe(gulp.dest('./BUM/'));
 });
 
-
-
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
-
-    browserSync({
-        proxy: "roccomarosi.dev"
-    });
-
-    gulp.watch("scss/*.scss", ['sass']);
-    gulp.watch("*.css").on('change', reload);
-    gulp.watch("*.htm").on('change', reload);
-    gulp.watch("*.js").on('change', reload);
-});
-
-
-gulp.task('sass', function () {
-    return gulp.src('scss/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('assets/css'))// Write CSS (& Source maps)?
-        .pipe(filter('**/*.css')) // Filtering stream to only css files
-        .pipe(browserSync.reload({stream:true}));
-});
-
-gulp.task('default', ['serve']);
-
-
-// to autoprefix just use sublime (alt+super+p) > autoprefixer
-// it will prefix the whole css/scss file
-// https://github.com/sindresorhus/sublime-autoprefixer
-
-//todo:
-//minify task at least for css then maybe also html
-//package task to zip version while in dev
+// SQUEEZE 'em
+gulp.task('bum', () =>
+    gulp.src('./BUM/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./BAM'))
+);
